a669728fc1621fbd3836d2ed6de25fd3843afbc3
hieuden0 <hieuden0@gmail.com>
2021-09-26 00:05:10 +0700

    create home page

---
+++ George.Web/Controllers/BaseSurfaceController.cs
@@ -0,0 +1,10 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace George.Web.Controllers
+{
+    public class BaseSurfaceController : SurfaceController    
+    {        
+        
+    }
+}
---
+++ George.Web/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace George.Web.Controllers
+{
+    public class HomeController : BaseSurfaceController
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
