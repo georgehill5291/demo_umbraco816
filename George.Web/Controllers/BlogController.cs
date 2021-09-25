5dd84e820d9f79b574881b1316636f9fb02a33db
George Hill <george.fxmediaweb@gmail.com>
2021-09-25 18:54:39 +0700

    frist init

---
+++ George.Web/Controllers/BlogController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace George.Web.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
